0cf99f95225071d9e559e654eff0583dfbb90efc
sudiptamondal1802 <38869305+sudiptamondal1802@users.noreply.github.com>
2021-06-25 14:52:11 +0100

    Delete DataVisualization directory

--- DataVisualization/BoxPlot.py
+++
@@ -1,7 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-#6.3 Group observations by the categorical feature and create one box plot of alcohol for each group
-plt.figure(figsize=(20,5))
-df.groupby('target')
-sns.boxplot(x='target', y='alcohol', data=df)
